e31f62cf062a2d28be6f2f5024f63ed368238e03
Aziel <azielcruzado@gmail.com>
2024-01-11 03:17:28 -0500

    Fix: Lines of problematic code removed

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-import './assets/main.css'
-=======
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import './style.css'
-
-import './demos/ipc'
-// If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
-// import './demos/node'
->>>>>>> 19995fcc1ff15259a0fc01b5ca03840f8b1f3f18
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import './assets/main.css'
+import './style.css'
 
 const app = createApp(App)
 
@@ -27,8 +18,8 @@ const directoryPhotosStorage = localStorage.getItem('directorySavePhotos');
 const directoryVideosStorage = localStorage.getItem('directorySaveVideos');
 window.electron.getDirectoryLocalStorage(directoryPhotosStorage, directoryVideosStorage)
 
-
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
