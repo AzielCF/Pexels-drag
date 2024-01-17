8b13acc52c8625eaee4a0fb72b7d65d7bd5ed7d0
Aziel <azielcruzado@gmail.com>
2024-01-17 18:56:50 -0500

    add: button ui component

---
+++ src/components/ui/elemets/buttonComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps({
+    valueName: String
+})
+</script>
+
+<template>
+    <div
+        class="bg-[#2b2b2b] hover:bg-[#3f3f3f] h-[42px] px-2 shadow-none rounded-sm cursor-pointer text-gray-50 flex items-center justify-center">
+        {{ valueName }}
+    </div>
+</template>
