9d69c2d8d4a32bd01444e4d8f44fd0450a6b0731
Aziel <azielcruzado@gmail.com>
2024-01-18 04:26:26 -0500

    fix: import name inputApiKeyComponent

--- src/components/ui/sections/popupApikeyComponent.vue
+++ src/components/ui/sections/popupApikeyComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Inputapikeycomponent from '../../inputapikeycomponent.vue'
+import Inputapikeycomponent from '../../inputApiKeyComponent.vue'
 
 </script>
 <template>
