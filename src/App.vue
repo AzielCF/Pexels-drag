73383c5b26614fda5a11c47ac8ef02f0936d98b3
Aziel <azielcruzado@gmail.com>
2024-01-18 04:07:10 -0500

    add: Api Key configuration & title bar added

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@ import { ref, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useSearchStore } from './stores/search'
 import { useRoute } from 'vue-router'
+import CustomTitleBarComponent from './components/customTitleBarComponent.vue'
+import PopupApikeyComponent from './components/ui/sections/popupApikeyComponent.vue'
 
 const store = useSearchStore()
-const { query, enter } = storeToRefs(store)
+const { query, apiKeyValue } = storeToRefs(store)
 const queryInit = ref('')
 const searcher = () => {
   store.query = queryInit.value
@@ -15,7 +17,6 @@ const searcher = () => {
   console.log('Buscar videos:', query.value);
   // Resto de tu código para buscar videos
 }
-
 
 const route = useRoute()
 
@@ -28,26 +29,30 @@ watch(() => route.path, (newPath, oldPath) => {
   // Aquí puedes realizar acciones específicas cuando se cambie de página
   console.log(`Cambiado de ${oldPath} a ${newPath}`)
 
-  electron.getDirectoryLocalStorage(directoryPhotosStorage, directoryVideosStorage)
+  window.electron.getDirectoryLocalStorage(directoryPhotosStorage, directoryVideosStorage)
 })
 </script>
+
 <template>
-  <header style="padding-top: 15px;">
-    <h1 class="logo">PEXELS DRAG</h1>
-    <p>Sientase libre de arrastrar cada elemento a cualquier programa o carpeta deseada.</p>
+  <CustomTitleBarComponent/>
+  <PopupApikeyComponent v-if="!apiKeyValue"/>
+
+  <header class="pt-10">
+    <h1 class="text-4xl">PEXELS DRAG</h1>
+    <p class="text-md pt-4">Sientase libre de arrastrar cada elemento a cualquier programa o carpeta deseada.</p>
     <div class="wrapper">
       <nav>
         <div class="search-bar">
-          <input class="dark-input" v-model="queryInit" @keydown.enter="searcher" placeholder="Buscar imagenes / videos">
+          <input class="dark-input" v-model="queryInit" @keydown.enter="searcher" placeholder="Buscar imágenes / vídeos">
         </div>
         <div style="padding: 15px 0px 20px 0px;">
           <RouterLink to="/">Fotos</RouterLink>
-          <RouterLink to="/videos">Videos</RouterLink>
+          <RouterLink to="/videos">Vídeos</RouterLink>
           <RouterLink to="/settings">Ajustes</RouterLink>
         </div>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <RouterView v-if="apiKeyValue"/>
 </template>
