3fd6cc2839288cb4be582887f8a4d82748f830e3
Aziel <azielcruzado@gmail.com>
2024-01-18 04:05:33 -0500

    add: popup apikey component

---
+++ src/components/ui/sections/popupApikeyComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Inputapikeycomponent from '../../inputapikeycomponent.vue'
+
+</script>
+<template>
+    <div class="configApiPopup flex flex-col items-center justify-center">
+        <header class="w-full py-5">
+            <h1 class="text-4xl">PEXELS DRAG</h1>
+            <h3 class="text-md pt-2">¡Bienvenido, para empezar ingrese su <a target="_blank"
+                    href="https://www.pexels.com/es-es/api/">Api Key de PEXELS</a> para funcionar!</h3>
+        </header>
+        <main class="w-full">
+            <Inputapikeycomponent/>
+        </main>
+    </div>
+</template>
