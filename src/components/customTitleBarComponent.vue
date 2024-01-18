25bc933cac6605150775ef6f21900554bbf4b824
Aziel <azielcruzado@gmail.com>
2024-01-18 03:47:09 -0500

    add: custom title bar

---
+++ src/components/customTitleBarComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="drag-title-bar fixed flex h-[40px] bg-[#333333] w-screen left-0 top-0 items-center">   
+        <div class="px-5">Pexels drag</div>
+    </div>
+</template>
+<style>
+.drag-title-bar{
+    -webkit-app-region: drag;
+    z-index: 9999;
+}
+</style>
