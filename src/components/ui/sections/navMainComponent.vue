046824d913ec7227da548ec97f80d90d09291c65
Aziel <azielcruzado@gmail.com>
2024-01-21 02:14:50 -0500

    add: template nav

---
+++ src/components/ui/sections/navMainComponent.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+    <nav class="flex place-content-center pb-6">
+        <RouterLink to="/">Fotos</RouterLink>
+        <RouterLink to="/videos">Vídeos</RouterLink>
+        <RouterLink to="/settings">Ajustes</RouterLink>
+    </nav>
+</template>
