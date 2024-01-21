32ce56392e03a9cb0af80be72cf28f874627188f
Aziel <azielcruzado@gmail.com>
2024-01-21 01:51:08 -0500

    Fix: Section labels are added

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -65,58 +65,62 @@ const applyPexelsApiKey = () => {
 
 <template>
   <main>
-    <h2 class="text-2xl">Ajustes</h2>
 
-    <div style="border-bottom: 1px solid #7a7a7a; margin: 20px 0px 20px 0px;" />
+    <section class="w-full pb-5">
+      <h2 class="text-2xl">Ajustes</h2>
 
-    <div class="section-input">
-      <label>Apikey</label>
-      <div class="section-directory">
-        <input class="dark-input" type="password" v-model="apiKeyValue" placeholder="Api Key">
-        <buttonComponent @click="applyPexelsApiKey()" valueName="Aplicar" />
+      <div style="border-bottom: 1px solid #7a7a7a; margin: 20px 0px 20px 0px;" />
+      <div class="section-input">
+        <label>Apikey</label>
+        <div class="section-directory">
+          <input class="dark-input" type="password" v-model="apiKeyValue" placeholder="Api Key">
+          <buttonComponent @click="applyPexelsApiKey()" valueName="Aplicar" />
+        </div>
       </div>
-    </div>
 
-    <div style="border-bottom: 1px solid #7a7a7a; margin: 20px 0px 20px 0px;" />
+      <div style="border-bottom: 1px solid #7a7a7a; margin: 20px 0px 20px 0px;" />
 
-    <div class="section-input">
-      <label>Directorio fotos</label>
-      <div class="section-directory">
-        <input class="dark-input" v-model="directoryPhotos" placeholder="Directorio">
-        <buttonComponent @click="selectDirectory('photos')" valueName="Seleccionar" />
+      <div class="section-input pb-4">
+        <label>Directorio fotos</label>
+        <div class="section-directory">
+          <input class="dark-input" v-model="directoryPhotos" placeholder="Directorio">
+          <buttonComponent @click="selectDirectory('photos')" valueName="Seleccionar" />
+        </div>
       </div>
-    </div>
-    <div class="section-input">
-      <label>Directorio vídeos</label>
-      <div class="section-directory">
-        <input class="dark-input" v-model="directoryVideos" placeholder="Directorio">
-        <buttonComponent @click="selectDirectory('video')" valueName="Seleccionar" />
+      <div class="section-input">
+        <label>Directorio vídeos</label>
+        <div class="section-directory">
+          <input class="dark-input" v-model="directoryVideos" placeholder="Directorio">
+          <buttonComponent @click="selectDirectory('video')" valueName="Seleccionar" />
+        </div>
       </div>
-    </div>
-
+    </section>
     <div style="border-bottom: 1px solid #7a7a7a; margin: 20px 0px 20px 0px;" />
-
-    <h2 class="text-2xl">Soporte</h2>
-    <div style="border-bottom: 1px solid #7a7a7a; margin: 20px 0px 20px 0px;" />
-    <a target="_blank" href="https://github.com/AzielCF/Pexels-drag">
-      <buttonComponent class="mt-4" valueName="Repositorio de Github" />
-    </a>
-    <buttonComponent class="mt-4" valueName="Página web" />
-    <a target="_blank" href="https://github.com/AzielCF/">
-      <buttonComponent class="mt-4" valueName="Autor del proyecto" />
-    </a>
+    <section class="w-full">
+      <h2 class="text-2xl">Soporte</h2>
+      <div style="border-bottom: 1px solid #7a7a7a; margin: 20px 0px 20px 0px;" />
+      <a target="_blank" href="https://github.com/AzielCF/Pexels-drag">
+        <buttonComponent class="mt-4" valueName="Repositorio de Github" />
+      </a>
+      <buttonComponent class="mt-4" valueName="Página web" />
+      <a target="_blank" href="https://github.com/AzielCF/">
+        <buttonComponent class="mt-4" valueName="Autor del proyecto" />
+      </a>
+    </section>
     <div class="mt-4" style="border-bottom: 1px solid #7a7a7a;" />
-
-    <div class="disclaimer pt-5">
+    <section class="disclaimer pt-5">
       <h2 class="text-2xl pb-2">Disclaimer</h2>
       <div style="border-bottom: 1px solid #7a7a7a; margin: 20px 0px 20px 0px;" />
       <p>Este programa utiliza la <a target="_blank" href="https://www.pexels.com/es-es/api/">API de Pexels</a> para
         obtener imágenes, pero no está afiliado, respaldado ni patrocinado por Pexels ni por sus desarrolladores
         originales. Todas las imágenes son propiedad de sus respectivos dueños y se proporcionan para uso personal y no
         comercial.</p>
-    </div>
+    </section>
     <div class="mt-4" style="border-bottom: 1px solid #7a7a7a;" />
-    <div class="pt-5">
-      Proyecto creado por <a target="_blank" href="https://github.com/AzielCF/">Aziel Cruzado</a>
-    </div>
-  </main></template>
+    <section>
+      <div class="pt-5">
+        Proyecto creado por <a target="_blank" href="https://github.com/AzielCF/">Aziel Cruzado</a>
+      </div>
+    </section>
+  </main>
+</template>
