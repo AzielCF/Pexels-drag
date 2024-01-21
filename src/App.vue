9edbf7afae26d4bab09fe8c70a56f97e71e46a27
Aziel <azielcruzado@gmail.com>
2024-01-21 01:49:48 -0500

    refactor: search engine logic moved to inpurSearcherComponent

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,44 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import { ref, watch } from 'vue'
+import { watch } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useSearchStore } from './stores/search'
 import { useRoute } from 'vue-router'
 import CustomTitleBarComponent from './components/customTitleBarComponent.vue'
 import PopupApikeyComponent from './components/ui/sections/popupApikeyComponent.vue'
+import InpurSearcherComponent from './components/inpurSearcherComponent.vue'
 
 const store = useSearchStore()
-const { query, apiKeyValue } = storeToRefs(store)
-const queryInit = ref('')
-const searcher = () => {
-  store.query = queryInit.value
-  // Lógica para buscar videos
-  window.electron.getDirectoryLocalStorage(directoryPhotosStorage, directoryVideosStorage)
-  console.log('Buscar videos:', query.value);
-  // Resto de tu código para buscar videos
-}
+const { apiKeyValue } = storeToRefs(store)
 
 const route = useRoute()
-
-// Puedes utilizar una referencia para mantener un seguimiento de la página actual
-
 const directoryPhotosStorage = localStorage.getItem('directorySavePhotos')
 const directoryVideosStorage = localStorage.getItem('directorySaveVideos')
-// Utiliza watch para detectar cambios en la propiedad $route
+
+// Esta logica mantiene el estado del medio descargado, para no volver a descargar
 watch(() => route.path, (newPath, oldPath) => {
   // Aquí puedes realizar acciones específicas cuando se cambie de página
   console.log(`Cambiado de ${oldPath} a ${newPath}`)
-
   window.electron.getDirectoryLocalStorage(directoryPhotosStorage, directoryVideosStorage)
 })
 </script>
 
 <template>
-  <CustomTitleBarComponent/>
-  <PopupApikeyComponent v-if="!apiKeyValue"/>
+  <CustomTitleBarComponent />
+  <PopupApikeyComponent v-if="!apiKeyValue" />
 
-  <header class="pt-10">
+  <header class="pt-10 pb-8 ">
     <h1 class="text-4xl">PEXELS DRAG</h1>
     <p class="text-md pt-4">Sientase libre de arrastrar cada elemento a cualquier programa o carpeta deseada.</p>
-    <div class="wrapper">
-      <nav>
-        <div class="search-bar">
-          <input class="dark-input" v-model="queryInit" @keydown.enter="searcher" placeholder="Buscar imágenes / vídeos">
-        </div>
-        <div style="padding: 15px 0px 20px 0px;">
-          <RouterLink to="/">Fotos</RouterLink>
-          <RouterLink to="/videos">Vídeos</RouterLink>
-          <RouterLink to="/settings">Ajustes</RouterLink>
-        </div>
-      </nav>
-    </div>
   </header>
+  <InpurSearcherComponent :routeState="route"/>
+  <nav class="flex place-content-center">
+    <div class="pb-6">
+      <RouterLink to="/">Fotos</RouterLink>
+      <RouterLink to="/videos">Vídeos</RouterLink>
+      <RouterLink to="/settings">Ajustes</RouterLink>
+    </div>
+  </nav>
 
-  <RouterView v-if="apiKeyValue"/>
+  <RouterView class="flex place-content-center flex-wrap text-center" v-if="apiKeyValue" />
 </template>
