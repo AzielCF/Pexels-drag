ff1b1f91b14f76283e6afde61fddae991702a238
Aziel <azielcruzado@gmail.com>
2024-01-21 02:14:23 -0500

    add: template header

---
+++ src/components/ui/sections/headerMainComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <header class="pt-10 pb-8 ">
+        <h1 class="text-4xl">PEXELS DRAG</h1>
+        <p class="text-md pt-4">Sientase libre de arrastrar cada elemento a cualquier programa o carpeta deseada.</p>
+    </header>
+</template>
