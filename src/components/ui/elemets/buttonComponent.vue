8b8ea8621d3f830f8a5ba9e66fc0ef36a6df0aa7
Aziel <azielcruzado@gmail.com>
2024-01-24 19:16:45 -0500

    add: icon slot space

--- src/components/ui/elemets/buttonComponent.vue
+++ src/components/ui/elemets/buttonComponent.vue
@@ -6,7 +6,7 @@ defineProps({
 
 <template>
     <div
-        class="bg-[#2b2b2b] hover:bg-[#3f3f3f] h-[42px] px-2 shadow-none rounded-sm cursor-pointer text-gray-50 flex items-center justify-center">
-        {{ valueName }}
+        class="bg-[#2b2b2b] hover:bg-[#3f3f3f] h-[42px] px-4 shadow-none rounded-sm cursor-pointer text-gray-50 flex items-center justify-center">
+        <div><slot name="icon"></slot></div>{{ valueName }}
     </div>
 </template>
