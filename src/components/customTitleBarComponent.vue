818dad79828eaaa59a0017abf572a08bc00346e9
Aziel <azielcruzado@gmail.com>
2024-01-25 21:50:26 -0500

    update padding x

--- src/components/customTitleBarComponent.vue
+++ src/components/customTitleBarComponent.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
     <div class="drag-title-bar fixed flex h-[40px] bg-[#333333] w-screen left-0 top-0 items-center">   
-        <div class="px-5">Pexels drag</div>
+        <div class="px-4 text-lg">Pexels drag</div>
     </div>
 </template>
 <style>
