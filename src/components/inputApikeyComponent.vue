59349f15925da4cd5179db9e16b9ab6815b24a98
Aziel <azielcruzado@gmail.com>
2024-01-30 01:19:46 -0500

    Refactor & add paste & toggleShowApikey

--- src/components/inputApikeyComponent.vue
+++ src/components/inputApikeyComponent.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
 const apiKey = ref('')
 const errorMessage = ref('') // Mensaje de error
 const isApiKeyValid = ref(false)
+const showApiKey = ref(false)
 
 const isValidApiKey = () => {
   // Verifica si la API key tiene al menos 50 caracteres y contiene al menos una letra mayúscula, una letra minúscula y un número
@@ -17,6 +18,9 @@ const applyPexelsApiKey = async () => {
   // Hacemos una url única con la fecha, ya que la búsqueda en caché puede darnos problemas
   const url = 'https://api.pexels.com/v1/search?query=ocean'
   const uniqueUrl = `${url}&_=${Date.now()}`
+
+  // Resetear el mensaje de error
+  errorMessage.value = ''
 
   if (isValidApiKey()) {
     // Realiza una solicitud a la API para validar la API key
@@ -39,12 +43,45 @@ const applyPexelsApiKey = async () => {
     errorMessage.value = 'Esto no es una API key válida' // Muestra el mensaje de error en la interfaz
   }
 }
+
+const pasteFromClipboard = async () => {
+  // Lógica para pegar desde el portapapeles
+  const textFromClipboard = await navigator.clipboard.readText()
+  apiKey.value = textFromClipboard
+  applyPexelsApiKey()
+}
+
+const toggleShowApiKey = () => {
+  // Lógica para mostrar u ocultar la clave
+  showApiKey.value = !showApiKey.value
+}
+
+onMounted(() => {
+  // Agregar un event listener para el evento keydown
+  window.addEventListener('keydown', handleKeyDown)
+})
+
+const handleKeyDown = (event: KeyboardEvent) => {
+  // Verificar si se presionaron las teclas Ctrl + Z
+  if (event.ctrlKey && event.key === 'z') {
+    // Resetear el contenido del input
+    apiKey.value = ''
+  }
+}
 </script>
 
-
 <template>
-  <div class="section-input text-center">
-    <input class="dark-input" v-model="apiKey" placeholder="Api Key" @input="applyPexelsApiKey()">
-    <div class="text-red-500 mt-5" v-show="apiKey">{{ errorMessage }}</div>
+  <div class="section-input flex max-w-screen-sm mx-auto">
+    <input class="dark-input" v-model="apiKey" placeholder="Api Key" @input="applyPexelsApiKey()"
+      :type="showApiKey ? 'text' : 'password'">
+    <div class="bg-[#2b2b2b] hover:bg-[#3f3f3f] h-[43px] shadow-none rounded-sm cursor-pointer items-center flex justify-center px-2 mx-1"
+      @click="pasteFromClipboard">
+      Pegar
+    </div>
+    <div class="bg-[#2b2b2b] hover:bg-[#3f3f3f] h-[43px] shadow-none rounded-sm cursor-pointer items-center flex justify-center px-2"
+      @click="toggleShowApiKey">
+      {{ showApiKey ? 'Ocultar' : 'Mostrar' }}
+    </div>
   </div>
+  <div class="text-red-500 mt-5 text-center" v-show="apiKey.trim() !== ''">{{ errorMessage }}</div>
 </template>
