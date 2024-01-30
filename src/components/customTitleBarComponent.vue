519d13f2a63f9b02995574a9f374425f5b8dab7d
Aziel <azielcruzado@gmail.com>
2024-01-29 21:13:05 -0500

    add logo in title bar

--- src/components/customTitleBarComponent.vue
+++ src/components/customTitleBarComponent.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import logo from '../components/ui/icons/logo.vue'
 </script>
 <template>
-    <div class="drag-title-bar fixed flex h-[40px] bg-[#333333] w-screen left-0 top-0 items-center">   
-        <div class="px-4 text-lg">Pexels drag</div>
+    <div class="drag-title-bar fixed flex h-[40px] bg-[#333333] w-screen left-0 top-0 items-center">         
+        <div class="flex flex-wrap items-center px-4 text-lg"><logo class="h-5 pr-2"/>Pexels drag</div>
     </div>
 </template>
 <style>
