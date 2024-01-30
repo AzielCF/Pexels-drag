37309957ff382995e8565ed7ce42fab399da3a09
Aziel <azielcruzado@gmail.com>
2024-01-29 21:55:50 -0500

    refactor automatic validation, apply button removed

--- src/components/inputApikeyComponent.vue
+++ src/components/inputApikeyComponent.vue
@@ -2,42 +2,49 @@
 import { ref } from 'vue'
 import axios from 'axios'
 
-const apiKey = ref()
+const apiKey = ref('')
 const errorMessage = ref('') // Mensaje de error
+const isApiKeyValid = ref(false)
+
+const isValidApiKey = () => {
+  // Verifica si la API key tiene al menos 50 caracteres y contiene al menos una letra mayúscula, una letra minúscula y un número
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
+  isApiKeyValid.value = apiKey.value.length >= 50 && regex.test(apiKey.value);
+  return isApiKeyValid.value;
+}
 
 const applyPexelsApiKey = async () => {
+  // Hacemos una url única con la fecha, ya que la búsqueda en caché puede darnos problemas
+  const url = 'https://api.pexels.com/v1/search?query=ocean'
+  const uniqueUrl = `${url}&_=${Date.now()}`
 
-    //Hacemos una url única con la fecha, ya que la busqueda en caché puede darnos problemas
-    const url = 'https://api.pexels.com/v1/search?query=ocean'
-    const uniqueUrl = `${url}&_=${Date.now()}`
-
-    if (apiKey.value && apiKey.value.length >= 50) {
-        // Realiza una solicitud a la API para validar la API key
-        try {
-            const response = await axios.get(uniqueUrl, {
-                headers: {
-                    Authorization: apiKey.value
-                }
-            })
-
-            // Si la solicitud es exitosa, guarda la API key en el localStorage y recarga la página
-            if (response.status === 200) {
-                localStorage.setItem('PEXELS_API_KEY', apiKey.value)
-                location.reload()
-            }
-        } catch (error) {
-            errorMessage.value = 'API key inválida.' // Muestra el mensaje de error en la interfaz
+  if (isValidApiKey()) {
+    // Realiza una solicitud a la API para validar la API key
+    try {
+      const response = await axios.get(uniqueUrl, {
+        headers: {
+          Authorization: apiKey.value
         }
-    } else {
-        errorMessage.value = 'Esto no es una API key' // Muestra el mensaje de error en la interfaz
+      })
+
+      // Si la solicitud es exitosa, guarda la API key en el localStorage y recarga la página
+      if (response.status === 200) {
+        localStorage.setItem('PEXELS_API_KEY', apiKey.value)
+        location.reload()
+      }
+    } catch (error) {
+      errorMessage.value = 'API key inválida.' // Muestra el mensaje de error en la interfaz
     }
+  } else {
+    errorMessage.value = 'Esto no es una API key válida' // Muestra el mensaje de error en la interfaz
+  }
 }
 </script>
 
+
 <template>
-    <div class="section-input text-center">
-        <input class="dark-input" v-model="apiKey" placeholder="Api Key">
-        <div class="bg-[#2b2b2b] hover:bg-[#3f3f3f] h-[35px] shadow-none rounded-md cursor-pointer items-center justify-center flex" @click="applyPexelsApiKey()">Aplicar</div>
-        <div class="text-red-500 mt-5">{{ errorMessage }}</div>
-    </div>
+  <div class="section-input text-center">
+    <input class="dark-input" v-model="apiKey" placeholder="Api Key" @input="applyPexelsApiKey()">
+    <div class="text-red-500 mt-5" v-show="apiKey">{{ errorMessage }}</div>
+  </div>
 </template>
